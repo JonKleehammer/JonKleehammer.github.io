b30db6b3ecd3ace1ec2c05a4b868109b407bc95c
Jon Kleehammer <Jon.Kleehammer@gmail.com>
2019-12-18 17:54:46 -0600

    Canvas DPI scaled to screen size, one problem is that we only scale it once when the web page is loaded. To scale dynamically for when users change the screen size we'll have to call for the screen scaling on every update

--- scripts.js
+++ scripts.js
@@ -1,3 +1,22 @@
+window.onload = function() {
+
+    SetupTypewriters();
+    SetupCanvas();
+};
+
+//region typewriter code
+
+function SetupTypewriters() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+}
+
 var TxtType = function(el, toRotate, period) {
     this.toRotate = toRotate;
     this.el = el;
@@ -39,13 +58,35 @@ TxtType.prototype.tick = function() {
     }, delta);
 };
 
-window.onload = function() {
-    var elements = document.getElementsByClassName('typewrite');
-    for (var i=0; i<elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-type');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-            new TxtType(elements[i], JSON.parse(toRotate), period);
-        }
+//endregion
+
+//region canvas code
+
+function SetupCanvas(){
+    //get DPI
+    let dpi = window.devicePixelRatio;
+    //get canvas
+    let canvas = document.getElementById('background-canvas');
+    //get context
+    let ctx = canvas.getContext('2d');
+
+    //get CSS height
+    //the + prefix casts it to an integer
+    //the slice method gets rid of "px"
+    let style_height = +getComputedStyle(canvas).getPropertyValue("height").slice(0, -2);
+    //get CSS width
+    let style_width = +getComputedStyle(canvas).getPropertyValue("width").slice(0, -2);
+    //scale the canvas
+    canvas.setAttribute('height', style_height * dpi);
+    canvas.setAttribute('width', style_width * dpi);
+
+    ctx.fillStyle = "red";
+    for(var i = 0; i < 100; i++){
+        var randX = style_width * Math.random();
+        var randY = style_height * Math.random();
+
+        ctx.fillRect(randX, randY, 10, 10)
     }
-};
+}
+
+//endregion
